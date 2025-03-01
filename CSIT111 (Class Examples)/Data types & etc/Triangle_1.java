1b993ce6e488eb67c7a9053a8d38d2bdbe1f7f9b
MFH97 <121143679+MFH97@users.noreply.github.com>
2025-03-01 19:41:59 +0800

    Create Triangle_1.java

---
+++ CSIT111 (Class Examples)/Data types & etc/Triangle_1.java
@@ -0,0 +1,40 @@
+// Read in the three sides of a triangle.
+// Compute and display its area and its perimeter
+//
+// File name: Triangle_1.java
+
+import java.util.Scanner;
+class Triangle_1
+{
+	public static void main (String [ ] args)
+	{
+		//declare variables
+		double a;
+		double b;
+		double c;
+		double area;
+		double perimeter;
+		
+		//construct a scanner class object input
+		Scanner input = new Scanner(System.in);
+		
+		//read in the 3 sides of the triangle
+		System.out.print("Enter the 3 sides: ");
+		a = input.nextDouble();
+		b = input.nextDouble();
+		c= input.nextDouble();
+		
+		//compute info
+		double s = (a + b + c) / 2.0;
+		area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+		perimeter = a + b + c;
+		
+		//display info
+		System.out.println("\nTriangle Info");
+		System.out.println("\ta  = " + a);
+		System.out.println("\tb = " + b);
+		System.out.println("\tc = " + c);
+		System.out.println("\tarea  = " + area);
+		System.out.println("\tperimeter  = " + perimeter);
+	}
+}
