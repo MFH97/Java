4cd88cb80e8143cacbf079daa06b7593632ef2de
MFH97 <121143679+MFH97@users.noreply.github.com>
2025-03-03 15:03:55 +0800

    Create Patterns.java

---
+++ Simple exercises/Patterns.java
@@ -0,0 +1,41 @@
+/******************************************************************************
+Use ONE System.out.println(...) 
+(print-line) statement for EACH LINE of outputs
+print the spaces too!
+*******************************************************************************/
+public class Patterns
+{
+	public static void main(String[] args) 
+	{   //Exercises using System.out.println()
+		System.out.println("Print Checker Pattern:");
+		System.out.println("\n***********");
+		System.out.println(" ***********");
+		System.out.println("***********");
+		System.out.println(" ***********");
+		System.out.println("***********");
+		
+		System.out.println("Print Square Pattern:");
+		System.out.println("*****");
+		System.out.println("*   *");
+		System.out.println("*   *");
+		System.out.println("*****");
+		System.out.println();
+		
+		System.out.println("Print Triangular Pattern:");
+		System.out.println();
+		System.out.println("* * * * *");
+		System.out.println(" *     *");
+		System.out.println("  *   *");
+		System.out.println("   * *");
+		System.out.println("    *");
+		System.out.println();
+		
+		System.out.println("Print Star Pattern:");
+		System.out.println();
+		System.out.println("  *  ");
+		System.out.println("** **");
+		System.out.println(" * *");
+		System.out.println("*   *");
+		//System.out.println("*    *");
+}
+}
